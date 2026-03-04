91cb5151055eb27e35fda8fd2c046ddf2fe5f0bf
Auri Gabriel <ririmello.a@gmail.com>
2026-03-04 17:07:56 -0300

    feat: update base path in vite.config.ts for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ const repoName = 'semana-da-arte-e-cultura';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? `/${repoName}/` : '/',
+  base: '/clextalegrete/ii-semana-da-arte-e-cultura/',
   plugins: [preact()],
   build: {
     outDir: 'dist',
